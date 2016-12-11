a73e459e646de67ad6d8d169d8df9f7e008707a1
Thomas Borghs <thomas.borghs@aca-it.be>
2016-12-11 21:26:06 +0100

    introducing a data model: part 2

--- src/app/library/album/overview/album.overview.component.ts
+++ src/app/library/album/overview/album.overview.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from "@angular/core";
 import {JsonRPCService} from "../../../services/jsonrpc/jsonrpc.service";
-import {LibraryService} from "../../service/library.service";
+import {LibraryService, Album} from "../../service/library.service";
 
 @Component({
   selector: 'ngp-album-overview',
@@ -8,13 +8,11 @@ import {LibraryService} from "../../service/library.service";
 })
 export class AlbumOverviewComponent implements OnInit {
 
-  albums: any[];
-  allAlbums: any[];
+  albums: Album[];
 
   constructor(private libraryService:LibraryService) { }
 
   ngOnInit() {
-    // this.libraryService.retrieveAlbumOverview().subscribe((data) => this.albums = data.result);
-    this.libraryService.getAlbums();
+    this.libraryService.getAlbums().subscribe((albumsResult) => this.albums = albumsResult);
   }
 }
--- src/app/library/service/library.service.ts
+++ src/app/library/service/library.service.ts
@@ -1,10 +1,9 @@
 import {JsonRPCService} from "../../services/jsonrpc/jsonrpc.service";
 import {Injectable} from "@angular/core";
-
-import 'rxjs/add/operator/mergeMap'
-import 'rxjs/add/operator/mergeAll'
-import 'rxjs/add/operator/reduce'
-import 'rxjs/Rx';
+import "rxjs/add/operator/mergeMap";
+import "rxjs/add/operator/mergeAll";
+import "rxjs/add/operator/reduce";
+import "rxjs/Rx";
 import {Observable} from "rxjs";
 
 const LIBRARY_BROWSE_METHOD = 'core.library.browse';
@@ -19,7 +18,6 @@ export class LibraryService {
   getAlbums() {
     return this.retrieveTrackDetails()
       .reduce(LibraryService.retrieveAlbums, [])
-      .subscribe((x) => console.log(x));
   }
 
   private retrieveTrackDetails() {
@@ -28,42 +26,42 @@ export class LibraryService {
       .flatMap(trackReferencesList => this.retrieveTrackDetailList(trackReferencesList));
   }
 
-  private recursivelyRetrieveAllTrackReferences(uri:string) {
+  private recursivelyRetrieveAllTrackReferences(uri: string) {
     if (uri.startsWith('local:directory')) {
       return this.jsonRpcService.performCall(LIBRARY_BROWSE_METHOD, {'uri': uri})
-        .flatMap((data:any) => data.result)
+        .flatMap((data: any) => data.result)
         .flatMap(directory => this.recursivelyRetrieveAllTrackReferences(directory.uri));
     } else {
       return Observable.of(uri);
     }
   }
 
-  private static collectAllTrackUris(collector:string[], uri) {
+  private static collectAllTrackUris(collector: string[], uri) {
     return collector.concat(uri);
   }
 
-  private retrieveTrackDetailList(trackReferences:string[]) {
+  private retrieveTrackDetailList(trackReferences: string[]) {
     return this.jsonRpcService.performCall(LIBRARY_LOOKUP_METHOD, {'uris': trackReferences})
       .flatMap(detailedTracksObject => Object.keys(detailedTracksObject.result).map(uriKey => detailedTracksObject.result[uriKey][0]));
   }
 
-  private static retrieveAlbums(collector:Album[], track) {
-    if (LibraryService.hasAlbum(track) && LibraryService.doesNotContainAlbumYet(collector, track.album.name)) {
-      collector.push(new Album(track.album.name, track.album.date, track.album.num_tracks));
+  private static retrieveAlbums(collector: Album[], track) {
+    if (LibraryService.hasAlbum(track) && LibraryService.hasArtists(track) && LibraryService.doesNotContainAlbumYet(collector, track.album.name, track.artists)) {
+      collector.push(new Album(track.album.name, track.album.date, track.album.num_tracks, track.artists));
     }
     return collector;
   }
 
-  private static hasAlbum(track):boolean {
+  private static hasAlbum(track): boolean {
     return track.album;
   }
 
-  private static doesNotContainAlbumYet(albumList:Album[], albumName:String) {
-    return albumList.every(album => album.name !== albumName);
+  private static hasArtists(track): boolean {
+    return track.artists;
   }
 
-  private static albumsAreSame(album1:Album, album2:Album):boolean {
-    return album1.name === album2.name;
+  private static doesNotContainAlbumYet(albumList: Album[], albumName: string, artistName: string) {
+    return albumList.every(album => album.name !== albumName);
   }
 
   retrieveAlbumDetails(directoryUri: string): any[] {
@@ -76,6 +74,7 @@ export class LibraryService {
   retrieveAlbumOverview() {
     return this.jsonRpcService.performCall(LIBRARY_BROWSE_METHOD, {'uri': 'local:directory'})
   }
+
 }
 
 export class Album {
@@ -83,16 +82,23 @@ export class Album {
   name: string;
   year: number;
   numberOfTracks: number;
+  artists: Artist[];
 
-  constructor(name: string, year: number, numberOfTracks: number) {
+  constructor(name: string, year: number, numberOfTracks: number, artists: Artist[]) {
     this.name = name;
     this.year = year;
     this.numberOfTracks = numberOfTracks;
+    this.artists = artists;
   }
 }
 
-export interface Artist {
+export class Artist {
+
   name: string
+
+  constructor(name: string) {
+    this.name = name;
+  }
 }
 
 export interface Track {
