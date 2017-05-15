04a9e23f4f50a12f5e4dfb2710e83c7a75e6ca2f
Thomas Borghs <thomas.borghs@aca-it.be>
2017-05-15 21:30:20 +0200

    backend playback: cleanup

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
-const ogg = require('ogg');
-const lame = require('lame');
 const fs = require('fs');
 const flac = require('flac-bindings');
 const Player = require('../player');
-
-// mp3
-// readStream.pipe(new lame.Decoder()).pipe(mySpeaker);
-
-// ogg - not working
-// readStream.pipe(new ogg.Decoder()).pipe(mySpeaker);
 
 const PCMSource = fs.createReadStream('/Users/thomasborghs/muziek/test.flac').pipe(new flac.StreamDecoder());
 const player = new Player(PCMSource);
